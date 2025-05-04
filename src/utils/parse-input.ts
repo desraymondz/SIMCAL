5e1d44cef1f3902f0f12a564044c02b59e16a100
desraymondz <desraymondz@gmail.com>
2025-05-05 00:26:30 +0800

    change algo to ignore new line \n

--- src/utils/parse-input.ts
+++ src/utils/parse-input.ts
@@ -1,6 +1,7 @@
 // Import types
 import { Schedule } from "@/types/schedule-type";
 import { Module } from "@/types/module-type";
+import removeEmptyLines from "@/utils/removeEmptyLines";
 
 // Converting input to an array of Module object
 export default function parse_input(input: string): Module[] {
@@ -9,10 +10,15 @@ export default function parse_input(input: string): Module[] {
         input = String(input);
     }
 
-    const lines = input.split('\n');
+    const clean_lines = removeEmptyLines(input);
+    const lines = clean_lines.split('\n');
     const input_len = input.length;
-    const module_splitter = 'Status	Units	Grading'
-    const holiday_splitter = 'Description	Date'
+    
+    const module_splitter = (line: string) =>
+        line.includes("Status") && line.includes("Units") && line.includes("Grading");
+
+    const holiday_splitter = (line: string) =>
+        line.includes("Description") && line.includes("Date");
 
     // Function to parse a single module
     function parse_one_module(start_i: number, end_i: number): Module {
@@ -20,7 +26,7 @@ export default function parse_input(input: string): Module[] {
         const class_number = lines[start_i + 7];
 
         const schedules: Schedule[] = [];
-        for (let i = start_i + 9; i < end_i; i += 7) {
+        for (let i = start_i + 9; i < end_i; i += 4) {
             const time_row = lines[i].split(' ');
             const day = time_row[0];
             const start_time = time_row[1];
@@ -36,7 +42,7 @@ export default function parse_input(input: string): Module[] {
                 end_time,
                 classroom,
                 lecturer,
-                date,
+                date
             };
 
             schedules.push(schedule);
@@ -52,8 +58,10 @@ export default function parse_input(input: string): Module[] {
 
     let module_indexes: number[] = [];
     for (let i = 0; i < input_len; i++) {
-        if (lines[i] === module_splitter || lines[i] === holiday_splitter) {
-            module_indexes.push(i - 1)
+        if (lines[i]){
+            if (module_splitter(lines[i]) || holiday_splitter(lines[i])) {
+                module_indexes.push(i - 1)
+            }
         }
     }
 
